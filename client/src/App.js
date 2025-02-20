c22d3f830970254acee635f220c4e0427ae429b2
Burak Guclu <burak.guclu@tedu.edu.tr>
2025-02-20 23:36:10 +0300

    Login and Signup skeletons created

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Login from "./components/Login";
+import SignUp from './components/SignUp';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <SignUp />
+      <Login />
+    </div>
   );
 }
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = () => { }
+
+    return (
+        <div className='login'>
+            <label>Login</label>
+            <input placeholder='Username' onChange={(event) => {
+                setUsername(event.target.value);
+            }} />
+            <input placeholder='Password' onChange={(event) => {
+                setPassword(event.target.value)
+            }} />
+            <button onClick={login}>Login</button>
+        </div>
+    )
 }
 
 export default Login
