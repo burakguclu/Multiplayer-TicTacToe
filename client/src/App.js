c5366a965542563a7edbd74172ed4cc2fe67ed55
Burak Guclu <burak.guclu@tedu.edu.tr>
2025-02-21 00:20:21 +0300

    Errors with cookies handled

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,26 @@ import './App.css';
 import Login from "./components/Login";
 import SignUp from './components/SignUp';
 import { StreamChat } from 'stream-chat'
+import Cookies from 'universal-cookie'
 
 function App() {
-  const api_key = process.env.api_key
-  const cookies = new Cookies()
-  const token = cookies.get("token")
-  const client = StreamChat.getInstance(api_key)
+  const api_key = process.env.REACT_APP_API_KEY
+  const cookies = new Cookies();
+  const token = cookies.get("token");
+  const client = StreamChat.getInstance(api_key);
 
   if (token) {
-    client.connectUser({
-      id: cookies.get("userId"),
-      name: cookies.get("username"),
-      firstName: cookies.get("firstName"),
-      lastName: cookies.get("lastName"),
-      hashedPassword: cookies.get("hashedPassword")
-    },
-      token).then((user) => {
+    client.connectUser(
+      {
+        id: cookies.get("userId"),
+        name: cookies.get("username"),
+        firstName: cookies.get("firstName"),
+        lastName: cookies.get("lastName"),
+        hashedPassword: cookies.get("hashedPassword")
+      },
+      token
+    )
+      .then((user) => {
         console.log(user)
       })
   }
