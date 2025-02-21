c5544ad33658d9ae4ab8cd4f0dab7c820e1002bd
Burak Guclu <burak.guclu@tedu.edu.tr>
2025-02-21 17:15:03 +0300

    Square and Board implemented

---
+++ client/src/components/Board.js
@@ -0,0 +1,26 @@
+import {React, useState} from 'react'
+import Square from './Square'
+
+function Board() {
+    const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""])
+
+    return <div className='board'>
+        <div className='row'>
+            <Square val={board[0]} />
+            <Square val={board[1]}/>
+            <Square val={board[2]}/>
+        </div>
+        <div className='row'>
+            <Square val={board[3]}/>
+            <Square val={board[4]}/>
+            <Square val={board[5]}/>
+        </div>
+        <div className='row'>
+            <Square val={board[6]}/>
+            <Square val={board[7]}/>
+            <Square val={board[8]}/>
+        </div>
+    </div>
+}
+
+export default Board
--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react'
+import Board  from './Board'
 
 function Game({ channel }) {
 
     const [playersJoined, setPlayersJoined] = useState(channel.state.watcher_count === 2)
 
+    channel.on("user.watching.start", (event) => {
+        setPlayersJoined(event.watcher_count === 2)
+    })
+
     if (!playersJoined) {
         return <div>Waiting for other player to join...</div>
     }
-    return <div>Game</div>
+    return <div className='gameContainer'>
+        <Board />
+        {/* CHAT */}
+        {/* LEAVE GAME BUTTON */}
+    </div>
 }
 
 export default Game
